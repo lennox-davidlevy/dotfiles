16997ace3e9937babdc3811112c3726836eb4e4f
David Levy <lennox.davidlevy@gmail.com>
2025-08-30 16:47:59 -0400

    add teardown while i test

---
+++ teardown.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "lets start again cause we be testing..."
+
+remove_packages() {
+  local packages=("$@")
+  for package in "${packages[@]}"; do
+    if rpm -q "$packages" &>/dev/null; then
+      echo "removing this bad boy: $package..."
+      sudo dnf remove -y "$package"
+    else
+      echo "$package not installed, skipping..."
+    fi
+  done
+}
+
+remove_packages "ansible" "git" "python3" "python3-pip"
+
+echo "clean up"
+sudo dnf autoremove -y
+
+echo "teardown done bro"
